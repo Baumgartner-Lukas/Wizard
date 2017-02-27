074762fca53e47fc19eb1c645bf4aa971fa31004
WebTree-Baumgartner <baumgartner.webtree@gmail.com>
2017-02-27 10:37:19 +0100

    Recommit: Project has: Working ViewPager with Tablayout, ic_launcher, background image

---
+++ app/src/main/java/com/baumgartner/webtree/wizard/Fragments/StartscreenFragment.java
@@ -0,0 +1,24 @@
+package com.baumgartner.webtree.wizard.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.baumgartner.webtree.wizard.R;
+
+/**
+ * Created by lbaum on 27.02.2017.
+ */
+
+public class StartscreenFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_startscreen, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/baumgartner/webtree/wizard/MainActivity.java
@@ -0,0 +1,24 @@
+package com.baumgartner.webtree.wizard;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.baumgartner.webtree.wizard.Fragments.ViewPagerFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPagerFragment fragment = new ViewPagerFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeholder, fragment);
+        fragmentTransaction.commit();
+    }
+}
